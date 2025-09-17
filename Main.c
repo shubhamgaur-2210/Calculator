6b21bf27d287969d8dcaa9a90c7133365f59fd8f
shubhamgaur-2210 <shubhamgaur6433@gmail.com>
2025-09-17 08:20:25 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,31 +1,66 @@
 #include <stdio.h>
 
 int main() {
+    char operator;
+    float first, second;
+    int num1, num2;
+    char choice;
+    printf("\n-------------------------------------------");
+    printf("\n-------------SHUBH'SCALCULATOR-------------");
 
-  char op;
-  double first, second;
-  printf("Enter an operator (+, -, *, /): ");
-  scanf("%c", &op);
-  printf("Enter two two numbers: ");
-  scanf("%lf %lf", &first, &second);
+    do {
+        do {
+            printf("\nEnter an operator (+, -, *, /, %%): ");
+            scanf(" %c", &operator);
 
-  switch (op) {
-    case '+':
-      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
-      break;
-    case '-':
-      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
-      break;
-    case '*':
-      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
-      break;
-    case '/':
-      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
-      break;
+            if (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%') {
+                printf("Invalid operator! Please try again.\n");
+            }
 
-    default:
-      printf("Error! operator is not correct");
-  }
+        } while (operator != '+' && operator != '-' && operator != '*' && operator != '/' && operator != '%');
 
-  return 0;
+        if (operator == '%') {
+            printf("\nEnter first integer: ");
+            scanf("%d", &num1);
+            printf("Enter second integer: ");
+            scanf("%d", &num2);
+
+            if (num2 == 0) {
+                printf("Error: Modulus by zero is not allowed.\n");
+            } else {
+                printf("\n %d %% %d = %d\n", num1, num2, num1 % num2);
+            }
+        }
+        else {
+            printf("\n============================================");
+            printf("\nEnter first number: ");
+            scanf("%f", &first);
+            printf("Enter second number: ");
+            scanf("%f", &second);
+
+            switch (operator) {
+                case '+':
+                    printf("%.2f + %.2f = %.2f\n", first, second, first + second);
+                    break;
+                case '-':
+                    printf("%.2f - %.2f = %.2f\n", first, second, first - second);
+                    break;
+                case '*':
+                    printf("%.2f * %.2f = %.2f\n", first, second, first * second);
+                    break;
+                case '/':
+                    printf("%.2f / %.2f = %.2f\n", first, second, first / second);
+                    break;
+            }
+        }
+    printf("\n============================================");
+        printf("\n Do you want to calculate again? (y/n): ");
+        scanf(" %c", &choice);
+
+    } while (choice == 'y' || choice == 'Y');
+    printf("\n============================================");
+    printf("\n Goodbye!\n");
+    printf("\n============================================");
+    return 0;
 }
+
